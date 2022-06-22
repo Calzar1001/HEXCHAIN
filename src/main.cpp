54a87bda06ef177981ee69764d5529d52cb36979
Calzar1001 <85040148+Calzar1001@users.noreply.github.com>
2022-06-22 13:43:21 -0600

    TouchPadClass
    @TODO
    Test Touch Pad With Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,16 @@
 #include <Arduino.h>
+#include "TouchPadNature.h"
+
+
+const int NUM_PADS=6;
+const int NO_PAD_SELECTED=-1;
+//Top row pin assignments
+const int fsrA=17;
+const int fsrB=15;
+const int fsrC=13;
+const int fsrD=16;
+const int fsrE=14;
+const int fsrF=12;
 
 void setup()
 {
